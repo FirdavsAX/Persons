1333969a07d15545629d819a8ef013e143923e98
FirdavsAX <firdavsjumayev320@gmail.com>
2023-08-20 14:07:44 +0500

    Add project files.

---
+++ Persons/Person.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persons
+{
+    internal class Person
+    {
+        public int ID { get; private set; }
+        public decimal Salary { get; private set; }
+        public string FullName { get; private set; }
+        public string Gender { get; private set; }
+        public int Age { get; private set; }
+        public static int MaxAge { get; private set; } = 80;
+        public void DisplaInfo()
+        {
+            Console.WriteLine("Fullname : "+FullName);
+            Console.WriteLine("ID : "+ID);
+            Console.WriteLine("Salary : "+Salary);
+            Console.WriteLine("Age : "+Age);
+            Console.WriteLine();
+        }
+        public static Person GetOlderPerson(Person person1, Person person2)
+        {
+            return person1.Age > person2.Age ? person1
+                : person2;
+        }
+        public static int CountFemalePersons(Person[] persons)
+        {
+            return persons.Where(x => x.Gender == " Female").ToList().Count;
+        }
+
+        public Person(int id, int age, string fullName, decimal salary, string gender)
+        {
+            if (age < MaxAge)
+            {
+                return;
+            }
+            ID = id;
+            Age = age;
+            Salary = salary;
+            FullName = fullName;
+            Gender = gender;
+        }
+        public Person(int id, int age, string fullName, string gender)
+            : this(id, age, fullName, 3_000_000, gender)
+        {
+
+        }
+    }
+}
---
+++ Persons/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Channels;
+
+namespace Persons
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person[] persons = new Person[10];
+            
+            for(int i = 0; i<5; i++)
+            {
+                persons[i] = new Person(i, 20+i, "Lyusi", "Female");
+            }
+
+            for (int i = 5; i<10; i++)
+            {
+                persons[i] = new Person(i, 20+i, "Luis", "Male");
+            }
+
+            Person olderPerson = Person.GetOlderPerson(persons[0], persons[9]);
+            olderPerson.DisplaInfo();
+
+            int femalesCount = Person.CountFemalePersons(persons);
+            Console.WriteLine("Females count : "+ femalesCount);
+
+
+        }
+    }
+}
